238e3a0004c0005abaa113b247424f7506517af1
D. Shikarev <dmitryshikarev@gmail.com>
2022-12-25 15:26:40 +0400

    fix: add styled to Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,8 +2,8 @@ import React, { useCallback } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useDispatch, useSelector } from 'react-redux'
 import { getUserAuthData, userActions } from 'entities/User'
-import { Button } from '@mui/material'
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage'
+import { LogoutButtonStyled } from 'widgets/Navbar/ui/Navbar.styled'
 import cls from './Navbar.module.scss'
 
 interface NavbarProps {
@@ -26,15 +26,14 @@ const Navbar = ({ className }: NavbarProps) => {
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <Button
+      <LogoutButtonStyled
         color="primary"
         variant="outlined"
-        sx={{ width: '130px' }}
         disableRipple
         onClick={onLogout}
       >
         Выход
-      </Button>
+      </LogoutButtonStyled>
     </div>
   )
 }
