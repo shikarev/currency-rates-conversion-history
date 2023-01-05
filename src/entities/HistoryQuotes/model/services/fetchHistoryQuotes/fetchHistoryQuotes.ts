e08a51a5b03d121f83d35057b734eb69d2160f0f
D. Shikarev <dmitryshikarev@gmail.com>
2023-01-05 16:23:00 +0400

    fix: remove cuid from fetchHistoryQuotes

--- src/entities/HistoryQuotes/model/services/fetchHistoryQuotes/fetchHistoryQuotes.ts
+++ src/entities/HistoryQuotes/model/services/fetchHistoryQuotes/fetchHistoryQuotes.ts
@@ -1,16 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { currencyApi } from 'shared/api'
 import { HistoryQuotesData } from 'entities/HistoryQuotes/model/types/history'
-import cuid from 'cuid'
 
 export const fetchHistoryQuotes = createAsyncThunk<HistoryQuotesData[], void, { rejectValue: string}>(
   'history/fetchHistoryQuotes',
   async (_, { dispatch, rejectWithValue }) => {
     try {
       const data = await dispatch(currencyApi.endpoints.history.initiate()).unwrap()
-      const historyTableData = data.deals.map((item) => ({ ...item, id: cuid() }))
+      const historyQuotesData = data.deals.map((item) => (
+        { ...item, id: item.asset + item.startDate + item.finishDate }
+      ))
 
-      return historyTableData
+      return historyQuotesData
     } catch (error) {
       console.log(error)
       return rejectWithValue('error')
